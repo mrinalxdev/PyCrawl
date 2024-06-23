a31a271b8f370e7f5b76848a7404eaf0767f55d0
Mrinal Pramanick <mrinalbitsat@gmail.com>
2024-06-24 03:05:06 +0530

    Simple File manager in Bash

---
+++ hardhat.config.js
@@ -0,0 +1,12 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.24",
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+};
