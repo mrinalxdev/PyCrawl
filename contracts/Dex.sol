7b8feb591235bcead0e152b61740ddf2f9bad1f7
Mrinal Pramanick <mrinalbitsat@gmail.com>
2024-06-24 14:31:38 +0530

    Dex app

--- contracts/Dex.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
-
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,5 @@
-require("@nomicfoundation/hardhat-toolbox");
-
+require("@nomicfoundation/hardhat-toolbox")
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
-  paths: {
-    sources: "./contracts",
-    tests: "./test",
-    cache: "./cache",
-    artifacts: "./artifacts"
-  },
 };
