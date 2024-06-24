3065ef11c8600f205cabda9a40109a9b5311e8d9
Mrinal Pramanick <mrinalbitsat@gmail.com>
2024-06-24 21:35:15 +0530

    Dex app sol file

--- contracts/Dex.sol
+++ contracts/Dex.sol
@@ -43,4 +43,51 @@ abstract contract IsDex is ReentrancyGuard, Ownable, ERC20 {
         token2 = IERC20(_token2);
         price = _initialPrice;
     }
+
+    function swapToken1forToken2(uint256 _amount) external nonReentrant {
+        require(_amount > 0, "Amount must be greater than 0");
+        uint256 token2Amount = (_amount * price) / PRICE_PRECISION;
+        uint256 fee = (token2Amount * FEE_PERCENTAGE) / FEE_DENOMINATOR;
+        uint256 token2AmountAfterFee = token2Amount - fee;
+
+        require(token2.balanceOf(address(this)) >= token2AmountAfterFee, "Insufficient Liquidity"); 
+
+        require(token1.transferFrom(msg.sender, address(this), _amount), "Transfer of token1 failed"); 
+        require(token2.transfer(msg.sender, token2AmountAfterFee), "Transfer of token2 failed");
+
+        totalToken1 += _amount;
+        totalToken2 -= token2AmountAfterFee;
+
+        _updatePrice();
+        emit Swap(msg.sender, _amount, token2AmountAfterFee, true);
+    }
+
+    function swapToken2forToken1(uint256 _amount) external nonReentrant {
+        require(_amount > 0, "Amount must be greater than 0");
+        uint256 token1Amount = (_amount * PRICE_PRECISION) / price;
+        uint256 fee = (token1Amount * FEE_PERCENTAGE) / FEE_DENOMINATOR;
+        uint256 token1AmountAfterFee = token1Amount - fee;
+
+        require(token1.balanceOf(address(this)) >= token1AmountAfterFee, "Insufficient Liquidity");
+        require(token2.transferFrom(msg.sender, address(this), _amount), "Transfer of token2 failed");
+        require(token1.transfer(msg.sender, token1AmountAfterFee), "Transfer of token1 failed");
+
+        totalToken2 += _amount;
+        totalToken1 -= token1AmountAfterFee;
+
+        _updatePrice();
+        emit Swap(msg.sender, token1AmountAfterFee, _amount, false);
+        
+    }
+
+    function addLiquidity(uint _tokenAmount, uint256 _token2Amount) external nonReentrant {
+        
+    }
+
+    function _updatePrice() internal {
+        if (totalToken1 > 0 && totalToken2 > 0) {
+            price = (totalToken2 * PRICE_PRECISION) / totalToken1;
+            emit PriceUpdated(price);
+        }
+    }
 }
